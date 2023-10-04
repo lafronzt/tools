ba352e735bfe0b1cf0a0cf381799d2dbe98de9c5
Tyler La Fronz <tyler@lafronz.com>
2023-10-04 18:00:52 -0400

    Split multiple IPs

--- netHelpers/addresses/ip.go
+++ netHelpers/addresses/ip.go
@@ -4,16 +4,18 @@ package addresses
 import (
 	"net"
 	"net/http"
+	"strings"
 )
 
 // GetIP returns the IP address of the client
 func GetRealIP(r *http.Request) *string {
+	var headerValue string
 	var remoteIP string
 
 	if len(r.Header.Get("X-REAL-IP")) > 0 {
-		remoteIP = r.Header.Get("X-REAL-IP")
+		headerValue = r.Header.Get("X-REAL-IP")
 	} else if len(r.Header.Get("X-Forwarded-For")) > 0 {
-		remoteIP = r.Header.Get("X-Forwarded-For")
+		headerValue = r.Header.Get("X-Forwarded-For")
 	} else {
 		host, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
@@ -21,5 +23,17 @@ func GetRealIP(r *http.Request) *string {
 		}
 		remoteIP = host
 	}
+
+	if len(headerValue) > 0 {
+		remoteIP = splitStringAndTakeFirstValue(headerValue)
+	}
+
 	return &remoteIP
+}
+
+func splitStringAndTakeFirstValue(str string) string {
+    if strings.Contains(str, ",") {
+        return strings.Split(str, ",")[0]
+    }
+    return str
 }
